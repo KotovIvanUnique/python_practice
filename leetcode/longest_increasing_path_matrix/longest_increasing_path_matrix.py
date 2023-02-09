06d7ba61f2f1810ee6349203207dbc566e40bbb0
Ivan <kotovivanunique@gmail.com>
2023-02-09 12:34:04 +0500

    Solved 329. Longest Increasing Path in a Matrix (https://leetcode.com/problems/longest-increasing-path-in-a-matrix/)

--- leetcode/longest_increasing_path_matrix/longest_increasing_path_matrix.py
+++ leetcode/longest_increasing_path_matrix/longest_increasing_path_matrix.py
@@ -1,29 +1,22 @@
 from typing import List
-from itertools import product
 
 class Solution:
-    def invalid(self, i, j, n, m):
-            return i < 0 or j < 0 or i == n or j == m
-
-    def make_move(self, matrix: List[List[int]], n: int, m: int, i: int, j: int, current_path: int, paths: List, dirs: List):        
-        # print('i', i, 'j', j, 'current', matrix[i][j], 'current_path', current_path)
-        for dx, dy in dirs:
-            r, c = i + dx, j + dy
-            if not self.invalid(r, c, n, m) and matrix[r][c] > matrix[i][j]:
-                self.make_move(matrix, n, m, r, c, current_path + 1, paths, dirs)
-            else:
-                paths.append(current_path + 1)
-
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
-        longest_path = 1
-        n, m = len(matrix), len(matrix[0])
-        for i, j in list(product(range(n), range(m))):
-            paths = []
-            dirs = [[1, 0], [0, 1], [-1, 0], [0, -1]]
-            self.make_move(matrix, n, m, i, j, 0, paths, dirs)
-            longest_path = max(longest_path, max(paths))
-        return longest_path
+        def dfs(i, j):
+            if not dp[i][j]:
+                val = matrix[i][j]
+                dp[i][j] = 1 + max(
+                    dfs(i - 1, j) if i and val > matrix[i - 1][j] else 0,
+                    dfs(i + 1, j) if i < M - 1 and val > matrix[i + 1][j] else 0,
+                    dfs(i, j - 1) if j and val > matrix[i][j - 1] else 0,
+                    dfs(i, j + 1) if j < N - 1 and val > matrix[i][j + 1] else 0)
+            return dp[i][j]
 
+        if not matrix or not matrix[0]: return 0
+        M, N = len(matrix), len(matrix[0])
+        dp = [[0] * N for i in range(M)]
+        return max(dfs(x, y) for x in range(M) for y in range(N))
+        
 def main():
     s = Solution()
     print('[[9,9,4],[6,6,8],[2,1,1]]', s.longestIncreasingPath([[9,9,4],[6,6,8],[2,1,1]]))
